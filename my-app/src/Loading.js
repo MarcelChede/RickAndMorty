367e9945d4afc3b8c2bc977bd2633ca3abfe8835
Marcel Chede <marcelchede@gmail.com>
2023-07-26 20:22:12 -0300

    Corrections on LoadingPage

--- my-app/src/Loading.js
+++ my-app/src/Loading.js
@@ -1,10 +1,12 @@
 import React from "react";
 import load from "./components/load.png"
+import logo from "./components/logo.png"
 import "./Loading_page.css";
 
 const LoadPage = () => {
   return (
     <div className="loadSetting">
+      <img className="LogoImagem" src={logo} alt = "LogoMain" />
       <img src={load} alt="Loading" />
       <p className="loadText">Loading</p>
     </div>
